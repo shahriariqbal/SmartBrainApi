6a25aff66a8401791b35ff90a8b04f7bbf5064e9
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-11 17:36:39 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : '1234',
+    database : 'smartbrain'
+  }
+});
+
+// db.select('*').from('users').then(data =>{
+// 	console.log(data);
+// });
+
+const app = express();
+
+
+
+
+app.use(cors())
+app.use(bodyParser.json());
+
+
+app.get('/', (req,res)=>{ res.send(database.users)  })
+
+// app.get('/', (req,res)=>{ res.send('it is working!')  })
+app.post('/signin', signin.handleSignin (db, bcrypt))
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=>{
+ console.log('app is running ');
+})
+
+// app.listen(process.env.PORT || 3000, ()=>{
+//  console.log(`app is running ${process.env.PORT} `);
+// })
